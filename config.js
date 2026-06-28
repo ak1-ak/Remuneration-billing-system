f8e69f9f596dee7f57ed107d46f265ce111f0092
ak1-ak <bavaliyaakash38@gmail.com>
2026-06-28 17:56:38 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "YOUR_ACTUAL_API_KEY_HERE",
-    authDomain: "remuneration-billing-system.firebaseapp.com",
-    databaseURL: "https://remuneration-billing-system-default-rtdb.firebaseio.com",
-    projectId: "remuneration-billing-system",
-    storageBucket: "remuneration-billing-system.firebasestorage.app",
-    messagingSenderId: "1026667021636",
-    appId: "1:1026667021636:web:ca5b385ab7aac73655c311"
-};
